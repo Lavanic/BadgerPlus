c4b39214dbe8eb51285e208ede9efdaf7a8055a9
Oliver Ohrt <oliver@theohrts.com>
2024-02-24 15:34:42 -0600

    SignIn Added

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ def about():
 def signup():
     return render_template('signup.html')
 
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
