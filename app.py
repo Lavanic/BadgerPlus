3bc1d4b154f9fa1ca80524cb6af68eddee0d9b88
Oliver Ohrt <oliver@theohrts.com>
2024-02-24 11:01:42 -0600

    First Init

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
